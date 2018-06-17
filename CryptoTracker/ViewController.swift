3094dced0cd533ca6bcd99ded273fc3b03958350
Christopher Czupak <czupak@Christophers-MacBook-Pro.local>
2018-06-17 13:29:37 -0400

    Initial Commit

---
+++ CryptoTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CryptoTracker
+//
+//  Created by Christopher Czupak on 6/17/18.
+//  Copyright © 2018 Christopher Czupak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
